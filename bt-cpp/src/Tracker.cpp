c9cd782df3f1725a3620445f1a542bd22c4b3657
Shree <shree.baskar@yahoo.com.au>
2024-09-07 12:42:12 +1000

    .

--- bt-cpp/src/Tracker.cpp
+++ bt-cpp/src/Tracker.cpp
@@ -149,39 +149,26 @@ std::string Tracker::categoryToString(Category category) {
         case Category::INVESTMENTS: return "Investments";
         case Category::GIFTS: return "Gifts";
         case Category::INTEREST: return "Interest";
-        default: return "Unknown";
+        default: return "Invalid Category";
     }
 }
 
 std::string Tracker::intToMonth(int month) {
     switch (month) {
-        case 1:
-            return "January";
-        case 2:
-            return "February";
-        case 3:
-            return "March";
-        case 4:
-            return "April";
-        case 5:
-            return "May";
-        case 6:
-            return "June";
-        case 7:
-            return "July";
-        case 8:
-            return "August";
-        case 9:
-            return "September";
-        case 10:
-            return "October";
-        case 11:
-            return "November";
-        case 12:
-            return "December";
+        case 1: return "January";
+        case 2: return "February";
+        case 3: return "March";
+        case 4: return "April";
+        case 5: return "May";
+        case 6: return "June";
+        case 7: return "July";
+        case 8: return "August";
+        case 9: return "September";
+        case 10: return "October";
+        case 11: return "November";
+        case 12: return "December";
+        default: return "Invalid Month";
     }
-
-    return "Invalid";
 }
 
 void Tracker::displayYearlyReport(int year) {
